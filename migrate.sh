d2004c66428f16310144ab5a870e12419f46f68a
Lukáš Hellmann <hellmann@vshosting.cz>
2024-08-07 20:32:43 +0200

    working recipe

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -o pipefail
+set -e
+
+export PGPASSWORD=${pgdb_password}
+
 INIT_DONE=$(psql -h pgdb -U pgdb pgdb -t -c "SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'db_migrations';" | tr -d '[:blank:]')
 if [[ ${INIT_DONE} -ne 1 ]]; then
     mattermost db init -c ${connectionString}
