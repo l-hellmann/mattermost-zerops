998c91de5b40dc02efe1cb42e737c5f8e99f10e3
Lukáš Hellmann <hellmann@vshosting.cz>
2024-08-09 14:17:50 +0200

    migrate.sh update psql command to use envs

--- migrate.sh
+++ migrate.sh
@@ -5,7 +5,7 @@ set -e
 
 export PGPASSWORD=${pgdb_password}
 
-INIT_DONE=$(psql -h pgdb -U pgdb pgdb -t -c "SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'db_migrations';" | tr -d '[:blank:]')
+INIT_DONE=$(psql -h ${pgdb_hostname} -U ${pgdb_user} pgdb -t -c "SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'db_migrations';" | tr -d '[:blank:]')
 if [[ ${INIT_DONE} -ne 1 ]]; then
     mattermost db init -c ${connectionString}
 fi
